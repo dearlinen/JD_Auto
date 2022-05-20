a4d32f002d0979181d45f30a629469f631f9a956
cyan <inseach@foxmail.com>
2022-05-20 11:42:56 +0800

    debug

--- index.js
+++ index.js
@@ -93,15 +93,10 @@ async function sendNotify() {
 
 
   function genTitle(str) {
-    console.log('str :>> ', str);
     if (str.match(/Cookie失效/)) {
       return '京东cookie失效，请更新'
     } else {
-      const result = /(?<=【签到奖励】:).+/m.exec(str)[0]
-      if (result) {
-        return `签到成功=>${result}`
-      }
-      return '签到失败,请查看GitHub Actions日志'
+        return `签到成功`
     }
   }
 
